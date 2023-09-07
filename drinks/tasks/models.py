356f101c271af7cce51e8ce4e5ab53144360d616
Seba <seba@local.pl>
2023-09-07 21:15:33 +0200

    Add model DRINKS to project and migrate to datadase

--- drinks/tasks/models.py
+++ drinks/tasks/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Drinks(models.Model):
+    drinkName = models.CharField(max_length=100, blank=False)
+    amount = models.IntegerField
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    desc = models.TextField(blank=True)
+    createDate = models.DateTimeField(auto_now_add=True, null=True)
