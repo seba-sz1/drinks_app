37c2444b5b9c8bab8e9ca0147e6f20628f69540d
Seba <seba@local.pl>
2023-09-10 20:25:52 +0200

    Repeir model Drinks by add relation ManyToMany with ingridients

--- drinks/tasks/models.py
+++ drinks/tasks/models.py
@@ -3,21 +3,15 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
-
 class Ingredient(models.Model):
     name = models.CharField(max_length=220)
-
-
-class IngredientToRecipeDrink(models.Model):
-    ingredient = models.ForeignKey(Ingredient, on_delete=models.SET_NULL)
     quantity = models.CharField(max_length=50, blank=True, null=True)  # 400
-    unit = models.CharField(max_length=50, blank=True, null=True)  # pounds, lbs, oz ,grams, etc
-    instructions = models.TextField(blank=True, null=True)
+    unit = models.CharField(max_length=50, blank=True, null=True)  # ml, l, szt
 
 
 class Drinks(models.Model):
     drinkName = models.CharField(max_length=100, blank=False)
-    ingredients = models.ManyToManyField(Ingredient,  blank=True, Null=True, through='IngredientToRecipeDrink')
+    ingredients = models.ManyToManyField(Ingredient, blank=True)
     amount = models.IntegerField(blank=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     desc = models.TextField(blank=True)
