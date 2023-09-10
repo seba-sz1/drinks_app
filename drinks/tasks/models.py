7c117f87932a5a2d1953a40729b07df07957d899
Seba <seba@local.pl>
2023-09-10 21:42:03 +0200

    Register models in admin site and update Drink model

--- drinks/tasks/models.py
+++ drinks/tasks/models.py
@@ -8,13 +8,20 @@ class Ingredient(models.Model):
     quantity = models.CharField(max_length=50, blank=True, null=True)  # 400
     unit = models.CharField(max_length=50, blank=True, null=True)  # ml, l, szt
 
+    def __str__(self):
+        return f'{self.name}  |  {self.quantity} {self.unit}'
 
-class Drinks(models.Model):
+
+class Drink(models.Model):
     drinkName = models.CharField(max_length=100, blank=False)
     ingredients = models.ManyToManyField(Ingredient, blank=True)
-    amount = models.IntegerField(blank=True)
+    amount = models.IntegerField(default=0)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     desc = models.TextField(blank=True)
     createDate = models.DateTimeField(auto_now_add=True, null=True)
 
+    def __str__(self):
+        return f'{self.drinkName} | {self.createDate} | {self.desc}'
 
+    #TODO -> jeżeli jednostki to ml to aktualizacja (quantity = sum)
+    # def ingredients_quantity(self):
