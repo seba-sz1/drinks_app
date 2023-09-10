f42b61ed09989fa9b46f841c5231135d0794482d
Seba <seba@local.pl>
2023-09-10 19:56:19 +0200

    Add simple view home templates, view hoome and define model recipe of drinks

--- drinks/tasks/models.py
+++ drinks/tasks/models.py
@@ -4,9 +4,23 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Ingredient(models.Model):
+    name = models.CharField(max_length=220)
+
+
+class IngredientToRecipeDrink(models.Model):
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.SET_NULL)
+    quantity = models.CharField(max_length=50, blank=True, null=True)  # 400
+    unit = models.CharField(max_length=50, blank=True, null=True)  # pounds, lbs, oz ,grams, etc
+    instructions = models.TextField(blank=True, null=True)
+
+
 class Drinks(models.Model):
     drinkName = models.CharField(max_length=100, blank=False)
-    amount = models.IntegerField
+    ingredients = models.ManyToManyField(Ingredient,  blank=True, Null=True, through='IngredientToRecipeDrink')
+    amount = models.IntegerField(blank=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     desc = models.TextField(blank=True)
     createDate = models.DateTimeField(auto_now_add=True, null=True)
+
+
--- drinks/tasks/views.py
+++ drinks/tasks/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def drinks(request):
+    print('DRINKS view works')
+    return None
