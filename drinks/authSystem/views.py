8f672f65ace2b08ec932c1075b491b1c57f9d293
Eliza Gancarz <elizaganca@gmail.com>
2023-09-11 21:20:13 +0200

    Add RegistrationForm validation, proper request method handling

--- drinks/authSystem/views.py
+++ drinks/authSystem/views.py
@@ -1,40 +1,42 @@
+from django.db import IntegrityError
 from django.shortcuts import render
 from .forms import RegisterForm
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseNotAllowed
 
 def temp_home(request):
     return render(request, 'temp_home.html')
 
-#TO DO: dodać sprawdzanie maila (czy to mail), ew. walidację hasła
 def register(request):
     if request.method == 'GET':
         return render(request,'register.html', {'form': RegisterForm()})
-    else:
-        username = request.POST.get('username')
-        email = request.POST.get('email')
-        password1 = request.POST.get('password1')
-        password2 = request.POST.get('password2')
-        if password1 == password2:
-            usernameTaken = User.objects.filter(username=username).exists()
-            emailTaken = User.objects.filter(email=email).exists()
-            if emailTaken:
-                error = 'This email is already taken. Try again.'
-            if usernameTaken:
-                error = 'This username is already taken. Try again.'
-            if not emailTaken and not usernameTaken:
-                user = User.objects.create_user(username, email, password1)
-                return render(request, 'temp_home.html')
+    elif request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if not form.is_valid():
+            error = form.errors
+        #TODO: Remove email validation when user model has unique email and handle it in IntegrityError.
+        elif User.objects.filter(email=form.cleaned_data['email']).exists():
+            error = 'This email is already taken. Try again.'
         else:
-            error = 'Wrong password. Try again'
-
+            try:
+                user = User.objects.create_user(form.username, form.email, form.password1)
+                return render(request, 'temp_home.html')
+            except IntegrityError as e:
+                # if 'UNIQUE constraint failed: auth_user.email' in e.args:
+                #     error = 'This user already exists.'
+                # else:
+                error = e
         return render(request, 'register.html', {'error': error, 'form': RegisterForm()})
+    else:
+        return HttpResponseNotAllowed(permitted_methods=['GET', 'POST'])
+
 
 def login_user(request):
     if request.method == 'GET':
         return render(request, 'login_user.html', {'form': AuthenticationForm()})
-    else:
+    elif request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
@@ -48,4 +50,7 @@ def login_user(request):
             else:
                 error = 'No such user in database.'
             return render(request, 'login_user.html', {'error': error, 'form': AuthenticationForm()})
+    else:
+        return HttpResponseNotAllowed(permitted_methods=['GET', 'POST'])
+
 
