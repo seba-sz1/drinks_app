3d4ec4a8a050bc14aa7f8331478bd12e43ebf24c
staz <m.kobialka@healthpharm.pl>
2023-09-11 18:52:26 +0200

    Changing the address of the base template

--- drinks/drinks/settings.py
+++ drinks/drinks/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'drinks.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'baseTemplates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
