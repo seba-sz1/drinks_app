9e4c8bb635cf84692971abe78a76b3246dd4e044
Eliza Gancarz <elizaganca@gmail.com>
2023-09-11 17:11:00 +0200

    add login method

--- drinks/authSystem/urls.py
+++ drinks/authSystem/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('', views.temp_home, name='home')
+    path('', views.temp_home, name='home'),
+    path('login/', views.login_user, name='login_user'),
+
 ]
--- drinks/authSystem/views.py
+++ drinks/authSystem/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from .forms import RegisterForm
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
 
 def temp_home(request):
     return render(request, 'temp_home.html')
 
-#TO DO: dodać sprawdzanie maila (czy to mail), walidację hasła
+#TO DO: dodać sprawdzanie maila (czy to mail), ew. walidację hasła
 def register(request):
     if request.method == 'GET':
         return render(request,'register.html', {'form': RegisterForm()})
@@ -29,4 +31,21 @@ def register(request):
 
         return render(request, 'register.html', {'error': error, 'form': RegisterForm()})
 
+def login_user(request):
+    if request.method == 'GET':
+        return render(request, 'login_user.html', {'form': AuthenticationForm()})
+    else:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return render(request, 'temp_home.html')
+        else:
+            usernameExist = User.objects.filter(username=username).exists()
+            if usernameExist:
+                error = 'Incorrect password.'
+            else:
+                error = 'No such user in database.'
+            return render(request, 'login_user.html', {'error': error, 'form': AuthenticationForm()})
 
