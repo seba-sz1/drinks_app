e6b6cf7c050e24278d721ec2d210de47a80a6e05
Seba <sebastianszczy@gmail.com>
2023-09-12 21:22:20 +0200

    Delete unused field from model drinks - ready to merge

--- drinks/tasks/models.py
+++ drinks/tasks/models.py
@@ -4,13 +4,13 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Ingredient(models.Model):
-    unit_choices = [('ml', 'mililitry'), ('szt', 'sztuk')]
-
+    unit_type = [('ml', 'mililitry'), ('szt', 'sztuk')]
     name = models.CharField(max_length=220)
     ingredient_amount = models.PositiveIntegerField(default=1,
-                                                    validators=[MinValueValidator(1), MaxValueValidator(300)],
+                                                    validators=[MinValueValidator(1),
+                                                                MaxValueValidator(300)],
                                                     blank=True)
-    unit = models.CharField(default='ml', choices=unit_choices, max_length=3)
+    unit = models.CharField(default='ml', choices=unit_type, max_length=3)
 
     def __str__(self):
         return f'{self.name}  |  {self.ingredient_amount} {self.unit}'
@@ -19,10 +19,10 @@ class Ingredient(models.Model):
 class Drink(models.Model):
     drink_name = models.CharField(max_length=100, blank=False)
     ingredients = models.ManyToManyField(Ingredient, blank=True)
-    size = models.IntegerField(null=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.TextField(blank=True)
     creation_date = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
-        return f'{self.drink_name} | {self.creation_date} | {self.size}'
+        return f'{self.drink_name} | {self.creation_date} | {self.description}'
+
