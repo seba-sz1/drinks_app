be88242fb8ac5a0b357fd5a85d1ed1fdb9da01f3
Seba <sebastianszczy@gmail.com>
2023-09-13 20:10:27 +0200

    Repair home template

--- drinks/authSystem/urls.py
+++ drinks/authSystem/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('', views.home, name='home'),
     path('login/', views.login_user, name='login_user'),
     path('logout/', views.logout_user, name='logout_user'),
 
