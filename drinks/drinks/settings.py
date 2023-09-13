04f8188c994d2b646c5f42da1f81f5b2bb1cfa63
staz <m.kobialka@healthpharm.pl>
2023-09-13 18:22:31 +0200

    Change url

--- drinks/drinks/settings.py
+++ drinks/drinks/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'drinks.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'Templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
