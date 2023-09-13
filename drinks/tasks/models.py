5bf9e1f3f7e3f3705eaf55285ab5edd85d5dda48
Seba <sebastianszczy@gmail.com>
2023-09-13 17:58:34 +0200

    Repeir model: change fieldsname and functions of fields

--- drinks/tasks/models.py
+++ drinks/tasks/models.py
@@ -4,25 +4,22 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Ingredient(models.Model):
-    unit_type = [('ml', 'mililitry'), ('szt', 'sztuk')]
+    unit_type = (('ml', 'mililitry'), ('szt', 'sztuk'))
     name = models.CharField(max_length=220)
-    ingredient_amount = models.PositiveIntegerField(default=1,
-                                                    validators=[MinValueValidator(1),
-                                                                MaxValueValidator(300)],
-                                                    blank=True)
+    amount = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(300)],
+                                         blank=True)
     unit = models.CharField(default='ml', choices=unit_type, max_length=3)
 
     def __str__(self):
-        return f'{self.name}  |  {self.ingredient_amount} {self.unit}'
+        return f'{self.name}  |  {self.amount} {self.unit}'
 
 
 class Drink(models.Model):
-    drink_name = models.CharField(max_length=100, blank=False)
-    ingredients = models.ManyToManyField(Ingredient, blank=True)
+    name = models.CharField(max_length=100, blank=False)
+    ingredients = models.ManyToManyField(Ingredient)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.TextField(blank=True)
-    creation_date = models.DateTimeField(auto_now_add=True, null=True)
+    creation_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.drink_name} | {self.creation_date} | {self.description}'
-
+        return f'{self.name} | {self.creation_date} | {self.description}'
