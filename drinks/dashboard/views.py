0962955fa2dffbcf5c6d8f46a75bf92f5a39c377
Seba <sebastianszczy@gmail.com>
2023-09-24 22:52:57 +0200

    Add column view of cocktails in the home page

--- drinks/dashboard/views.py
+++ drinks/dashboard/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from cocktails.models import Drink
 
 
 @login_required()
 def dashboard(request):
-    # productUser = Drink.objects.filter(user=request.user)
     return render(request, 'dashboard.html')
