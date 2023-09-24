41baea7bc7957cad93a56138d61c5d91d3636768
Seba <sebastianszczy@gmail.com>
2023-09-24 22:52:57 +0200

    Add column view of cocktails in the home page

--- drinks/cocktails/views.py
+++ drinks/cocktails/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Drink
+from django.utils import timezone
 
 
 def home(request):
-    return render(request, 'home.html')
+    last_added_cocktails = Drink.objects.order_by('-creation_date')[:6]
+    first_half = last_added_cocktails[:3]
+    second_half = last_added_cocktails[3:]
+
+    return render(request, 'home.html',
+                  {'last_cocktails1': first_half, 'last_cocktails2': second_half})
