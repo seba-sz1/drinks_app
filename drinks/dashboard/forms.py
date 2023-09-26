c1efa270fae537272bcbf68f19aa22fa7edb667b
staz <m.kobialka@healthpharm.pl>
2023-09-27 00:43:37 +0200

    Add igredient form

--- drinks/dashboard/forms.py
+++ drinks/dashboard/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from cocktails.models import Drink, Ingredient
 
+
 class AddDrink(forms.ModelForm):
     name = forms.CharField(
         label='Nazwa drinka',
@@ -11,17 +12,19 @@ class AddDrink(forms.ModelForm):
         label='Krótki opis',
         widget=forms.TextInput(attrs={'class': 'form-control'}),
     )
-    ingredients_query = Ingredient.objects.all()
-    INGREDIENT_CHOICES = [(ingredient.name, ingredient.name) for ingredient in ingredients_query]
-
-    ingredients = forms.MultipleChoiceField(
-        label='Wybierz składniki',
-        choices=INGREDIENT_CHOICES,
-        widget=forms.CheckboxSelectMultiple(attrs={'class': 'custom-checkbox-class'}),
-    )
 
 
-    image=forms.ImageField(
+    # ingredients_query = Ingredient.objects.all()
+    # INGREDIENT_CHOICES = [(ingredient.name, ingredient.name) for ingredient in ingredients_query]
+    #
+    # ingredients = forms.MultipleChoiceField(
+    #     label='Wybierz składniki',
+    #     choices=INGREDIENT_CHOICES,
+    #     widget=forms.CheckboxSelectMultiple(attrs={'class': 'custom-checkbox-class'}),
+    # )
+
+
+    image = forms.ImageField(
         label='Dodaj zdjęcie',
         widget=forms.ClearableFileInput(attrs={'class': 'form-control', 'accept': 'image/*'}),
 
@@ -29,11 +32,11 @@ class AddDrink(forms.ModelForm):
 
     drink_publish = forms.BooleanField(
         label='Drink publiczny',
-        required=True,  # Jeśli pole ma być opcjonalne
+        required=False,  # Jeśli pole ma być opcjonalne
         widget=forms.CheckboxInput(attrs={'class': 'custom-checkbox-class'}),
     )
     class Meta:
         model = Drink
-        exclude = ('creation_date', 'owner', 'thumbnail')
+        fields = ['name', 'description', 'image', 'drink_publish']
 
 
--- drinks/dashboard/views.py
+++ drinks/dashboard/views.py
@@ -15,16 +15,32 @@ def dashboard(request):
 @login_required()
 def create(request):
     if request.method == 'GET':
-        return render(request, 'create.html', {'form' : AddDrink()})
+        available_ingredients = Ingredient.objects.all()
+        return render(request, 'create.html', {'form': AddDrink(), 'ingredients': available_ingredients})
+
     elif request.method == 'POST':
-        form = AddDrink(request.POST)
+        form = AddDrink(request.POST, request.FILES)
         if form.is_valid():
             drink = form.save(commit=False)
             drink.owner = request.user
-            drink.save()
+            drink.save()  # Zapisz drinka do bazy danych
+
+            selected_ingredients = []
+            # Obsłuż składniki
+            for key, value in request.POST.items():
+                if key.startswith('ingredient_'):
+                    ingredient_id = int(key.split('_')[1])
+                    # ingredient = Ingredient.objects.get(pk=ingredient_id)
+                    # selected_ingredient_id = form.cleaned_data['ingredient']
+                    # selected_ingredient_id = request.POST.get('ingredient')
+                    selected_ingredients.append(ingredient_id)
+
+            # Przypisz składniki do drinka
+            drink.ingredients.set(selected_ingredients)
             return redirect('dashboard')
+
         else:
             error = 'Coś poszło nie tak!'
-            return render(request, 'create.html', {'form': AddDrink(), 'error': error})
+            return render(request, 'create.html', {'form': form, 'error': error})
     else:
         return HttpResponseNotAllowed(permitted_methods=['GET', 'POST'])
