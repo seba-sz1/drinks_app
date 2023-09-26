17401290a1af4ab408579639f5d4bc317157cadc
staz <m.kobialka@healthpharm.pl>
2023-09-26 20:43:42 +0200

    Rebase main

--- drinks/cocktails/models.py
+++ drinks/cocktails/models.py
@@ -1,18 +1,27 @@
+import random
+import string
+import os
+
+from PIL import Image
+from io import BytesIO
 from django.db import models
+from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
 
+def random_string(length):
+    """Generates a random string of given length."""
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for _ in range(length))
 
 class Ingredient(models.Model):
     unit_type = (('ml', 'mililitry'), ('szt', 'sztuk'))
     name = models.CharField(max_length=220)
-    amount = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(300)],
-                                         blank=True)
+    amount = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(300)], blank=True)
     unit = models.CharField(default='ml', choices=unit_type, max_length=3)
 
     def __str__(self):
         return f'{self.name}  |  {self.amount} {self.unit}'
-
 
 class Drink(models.Model):
     name = models.CharField(max_length=100, blank=False)
@@ -20,7 +29,43 @@ class Drink(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.TextField(blank=True)
     creation_date = models.DateTimeField(auto_now_add=True)
-    image = models.ImageField(upload_to='static/images/', default='static/images/1.jpg')
+    image = models.ImageField(upload_to='images/', default='images/1.jpg')
+    thumbnail = models.ImageField(upload_to='thumbnails/', blank=True, null=True)
+    public = models.BooleanField(default=True)
+
+    def save(self, *args, **kwargs):
+        # Tworzenie miniaturki (thumbnail) na podstawie obrazu przy każdym zapisie
+        self.thumbnail = self.create_thumbnail()
+
+        super(Drink, self).save(*args, **kwargs)
+
+    def create_thumbnail(self):
+        # Otwórz obraz główny
+        image = Image.open(self.image)
+
+        # Ustaw maksymalny rozmiar miniaturki (300x200)
+        max_size = (300, 200)
+
+        # Warunek, który sprawdzi, czy obraz jest większy niż maksymalny rozmiar
+        if image.width > max_size[0] or image.height > max_size[1]:
+            # Skaluj obraz zachowując proporcje
+            image.thumbnail(max_size, Image.LANCZOS)
+
+        # Tworzenie miniaturki
+        thumbnail_io = BytesIO()
+        image.save(thumbnail_io, 'JPEG', quality=85)
+
+        # Wygeneruj losową nazwę dla miniaturki
+        thumbnail_filename = random_string(10) + '.jpg'
+
+        # Zapisz miniaturkę z losową nazwą w katalogu "thumbnails/"
+        thumbnail_path = thumbnail_filename
+        self.thumbnail.save(thumbnail_path,
+                            InMemoryUploadedFile(thumbnail_io, None, thumbnail_path, 'image/jpeg', thumbnail_io.tell(),
+                                                 None), save=False)
+
+        return self.thumbnail
+
 
     def __str__(self):
         return f'{self.name} | {self.creation_date} | {self.description}'
--- drinks/drinks/settings.py
+++ drinks/drinks/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static"
@@ -135,3 +135,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 LOGIN_URL = '/login/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+
+
