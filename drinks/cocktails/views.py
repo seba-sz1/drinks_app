b3be406857188b7f8cefd77c8c520642f4b5c5c6
Seba <sebastianszczy@gmail.com>
2023-09-27 18:06:56 +0200

    Upgrade base carousel by display cocktail marked with pin_home_page=True

--- drinks/cocktails/views.py
+++ drinks/cocktails/views.py
@@ -6,9 +6,10 @@ from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 def home(request):
     last_added_cocktails = Drink.objects.filter(drink_publish=True).order_by('-creation_date')[:6]
-    first_half = last_added_cocktails[:3]
-    second_half = last_added_cocktails[3:] if len(last_added_cocktails[3:]) >= 2 else False
-    pined_home = Drink.objects.filter(pin_to_main_page=True)
+    first_half = last_added_cocktails[:3] if len(last_added_cocktails[:3]) >= 2 else None
+    second_half = last_added_cocktails[3:] if len(last_added_cocktails[3:]) >= 2 else None
+    pined_home = Drink.objects.filter(pin_to_main_page=True, drink_publish=True)
+    pined_home = pined_home if len(pined_home) > 1 else None
 
     return render(request, 'home.html',
                   {'last_cocktails1': first_half, 'last_cocktails2': second_half, 'pined_home': pined_home})
@@ -20,7 +21,7 @@ def search(request):
     drinks = drinks.distinct()
     total_count = drinks.count()
     last_added_cocktails = Drink.objects.filter(drink_publish=True).order_by('-creation_date')
-    last_added_cocktails = last_added_cocktails[:3] if len(last_added_cocktails[:3]) >= 2 else False
+    last_added_cocktails = last_added_cocktails[:3] if len(last_added_cocktails[:3]) >= 2 else None
 
     paginator = Paginator(drinks, 8)
     page_number = request.GET.get('page')
@@ -40,5 +41,5 @@ def search(request):
 def detail_cocktail(request, drinkID):
     drink = get_object_or_404(Drink, id=drinkID)
     last_added_cocktails = Drink.objects.filter(drink_publish=True).order_by('-creation_date')
-    last_added_cocktails = last_added_cocktails[:3] if len(last_added_cocktails[:3]) >= 2 else False
+    last_added_cocktails = last_added_cocktails[:3] if len(last_added_cocktails[:3]) >= 2 else None
     return render(request, 'cocktail_detail.html', {'drink': drink, 'last_cocktails': last_added_cocktails})
